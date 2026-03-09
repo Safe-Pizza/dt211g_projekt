a7b9f0b109261ca459ef69ed54114ff4419c6754
Hanna Lindkvist <hali2507@student.miun.se>
2026-03-09 14:34:55 +0100

    koppla js fil till html

--- src/js/api.js
+++ src/js/api.js
@@ -1,6 +1,6 @@
 "use strict";
 
-//hämta JSON-data
+//hämta JSON-data vattenfontäner
 async function fetchData() {
     try {
         const response = await fetch("https://services-eu1.arcgis.com/81H0sgjoIWj6WxIM/arcgis/rest/services/Dricksvattenfont%C3%A4ner/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json");
