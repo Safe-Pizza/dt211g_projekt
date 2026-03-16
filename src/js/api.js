4e228074c42d38f7950ac52aace03d96c5d2bc4c
Hanna Lindkvist <hali2507@student.miun.se>
2026-03-16 14:19:47 +0100

    skriver funktion för att hämta adress + karta

--- src/js/api.js
+++ src/js/api.js
@@ -37,6 +37,18 @@ async function fetchUniqueData(uniqueData) {
     }
 }
 
+async function fetchLocationData(street) {
+    try {
+        const res = await fetch(`https://nominatim.openstreetmap.org/search?q=${street}&format=jsonv2`);
+        const data = await res.json();
+
+        return data;
+
+    } catch (error) {
+        document.querySelector("#map").innerHTML = "Platsen finns ej, prova sök igen";
+    }
+}
+
 function writeData(data) {
     const resultBeachEl = document.querySelector("#result-beach");
     resultBeachEl.innerHTML = "";
@@ -60,7 +72,8 @@ function writeData(data) {
 
         //eventlyssnare för klick(
         articleEl.addEventListener("click", async () => {
-            writeUniqueData(d.links.self)
+            writeUniqueData(d.links.self);
+            showMap(d.attributes.address.street);
         })
     })
 
@@ -72,23 +85,40 @@ async function writeUniqueData(uniqueBeachData) {
     const resultBeachEl = document.querySelector("#result-beach");
     resultBeachEl.innerHTML = "";
 
-        const articleEl = document.createElement("article");
-        const h3El = document.createElement("h3");
-        const h4El = document.createElement("h4");
-        const pEl = document.createElement("p");
+    const articleEl = document.createElement("article");
+    const h3El = document.createElement("h3");
+    const h4El = document.createElement("h4");
+    const pEl = document.createElement("p");
+    const p2El = document.createElement("p");
 
-        //lägg till text
-        h3El.innerHTML = beachData.attributes.name.toUpperCase();
-        h4El.innerHTML = beachData.attributes.address.street.toUpperCase() + ', ' + beachData.attributes.address.city.toUpperCase() + '<span class="fa-solid fa-location-dot"</span>';
-        pEl.innerHTML = beachData.attributes.shortDescription;
+    //lägg till text
+    h3El.innerHTML = beachData.attributes.name.toUpperCase();
+    h4El.innerHTML = beachData.attributes.address.street.toUpperCase() + ', ' + beachData.attributes.address.city.toUpperCase() + '<span class="fa-solid fa-location-dot"</span>';
+    pEl.innerHTML = beachData.attributes.shortDescription;
+    p2El.innerHTML = beachData.attributes.contentSections;
 
-        //lägg till i article
-        articleEl.appendChild(h3El);
-        articleEl.appendChild(h4El);
-        articleEl.appendChild(pEl);
+    //lägg till i article
+    articleEl.appendChild(h3El);
+    articleEl.appendChild(h4El);
+    articleEl.appendChild(pEl);
 
-        //skriv ut till DOM
-        resultBeachEl.appendChild(articleEl);
+    //skriv ut till DOM
+    resultBeachEl.appendChild(articleEl);
+}
+
+async function showMap(address) {
+    const addressArr = await fetchLocationData(address);
+    const mapEl = document.querySelector("#map");
+
+    //latitud och longitud från första objektet i sökning
+    const lat = addressArr[0].lat;
+    const lon = addressArr[0].lon;
+
+    //variabel för kart-url
+    const mapUrl = `https://www.openstreetmap.org/export/embed.html?bbox=${lon},${lat},${lon},${lat}&amp;layer=mapnik&amp;marker=${lat},${lon}`;
+
+    //skriva ut karta till DOM
+    mapEl.innerHTML = `<iframe class="responsive-iframe" width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="${mapUrl}"></iframe>`
 }
 
 function searchFilter(dataArr) {
